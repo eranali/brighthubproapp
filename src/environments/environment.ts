35efac5cf1a2636ae2b74fc75b3e2f4f24cf0523
Gabriel <gabrielfuja001@gmail.com>
2021-01-18 11:19:16 +0000

    updates

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebaseConfig: {
     apiKey: 'AIzaSyBbcJ1aJkJOWc0OgT7owt9mlc8ZkNW5ZmQ',
     authDomain: 'https://brighthubpro-41a4c.firebaseio.com',
